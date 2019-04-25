961659ca4b336f409997789c282402f9d402834c
Akshat Sharma <akshatsharma46@gmail.com>
2019-04-24 18:57:47 -0700

    removed curl from allUsers

--- allUsers.php
+++ allUsers.php
@@ -47,23 +47,6 @@
     }
 
     $conn->close();
-
-    // create curl resource
-    $ch = curl_init();
-
-    // set url
-    curl_setopt($ch, CURLOPT_URL, "www.roopeshrai.com/myusers.php");
-
-    //return the transfer as a string
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-
-    // $output contains the output string
-    $output = curl_exec($ch);
-
-    echo $output;
-
-    // close curl resource to free up system resources
-    curl_close($ch);
     ?>
 
 </body>
