e43900ce71730d00889b1276c1d6f39593ce316c
Akshat Sharma <akshatsharma46@gmail.com>
2019-04-24 18:55:22 -0700

    curl test

--- allUsers.php
+++ allUsers.php
@@ -47,6 +47,23 @@
     }
 
     $conn->close();
+
+    // create curl resource
+    $ch = curl_init();
+
+    // set url
+    curl_setopt($ch, CURLOPT_URL, "www.roopeshrai.com/myusers.php");
+
+    //return the transfer as a string
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+    // $output contains the output string
+    $output = curl_exec($ch);
+
+    echo $output;
+
+    // close curl resource to free up system resources
+    curl_close($ch);
     ?>
 
 </body>
