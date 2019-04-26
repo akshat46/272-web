eaed4cbbeec55938fee67b86470d67871ab9601d
Akshat Sharma <akshatsharma46@gmail.com>
2019-04-25 18:52:03 -0700

    testing curl

--- curl.php
+++ curl.php
@@ -4,7 +4,52 @@
 
     include 'allUsers.php';
 
-    echo 'Hello World';
+    $servername = "localhost";
+    $username = "leap-user";
+    $password = "leap123456";
+    $dbname = "leap";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Database Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "SELECT * FROM users";
+    if ($result = $conn->query($sql)) {
+        if ($result->num_rows > 0) {
+            echo "<h2>All Users from Akshat's application:</h2>";
+            echo '<table class="zui-table">';
+            echo "<tr>";
+            echo "<th>Firstname</th>";
+            echo "<th>Lastname</th>";
+            echo "<th>Email</th>";
+            echo "<th>Address</th>";
+            echo "<th>Home Phone</th>";
+            echo "<th>Cell Phone</th>";
+            echo "</tr>";
+            while ($row = $result->fetch_assoc()) {
+                //echo '<pre>'; print_r($array); echo '</pre>';
+                echo "<tr>";
+                echo "<td>".$row['first_name']."</td>";
+                echo "<td>".$row['last_name']."</td>";
+                echo "<td>".$row['email']."</td>";
+                echo "<td>".$row['address']."</td>";
+                echo "<td>".$row['home_phone']."</td>";
+                echo "<td>".$row['cell_phone']."</td>";
+                echo "</tr>";
+            }
+            echo "</table>";
+            mysqli_free_res($result);
+        }
+        else {
+            echo "No users found.";
+        }
+    }
+
+    $conn->close();
     // $ch = curl_init("http://brewgy.com/myUsers.php");
     // echo "testing 52";
     // curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
