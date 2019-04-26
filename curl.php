3e9081e0373ac0e9a7b7f3c838af2d463a314416
Akshat Sharma <akshatsharma46@gmail.com>
2019-04-25 20:10:31 -0700

    modulated curl

--- curl.php
+++ curl.php
@@ -1,13 +1,15 @@
 <?php
 
-    include 'allUsers.php';
+include 'allUsers.php';
 
-    echo "testing curl";
+$urls = array("http://www.roopeshrai.com/myusers.php", "http://brewgy.com/myUsers.php");
+
+foreach ($urls as &$url) {
     $ch = curl_init();
-    echo "testign curl2";
-    curl_setopt($ch, CURLOPT_URL, "http://www.roopeshrai.com/myusers.php");
+    curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     $contents = curl_exec ($ch);
     echo $contents;
     curl_close ($ch);
+}
 ?>
