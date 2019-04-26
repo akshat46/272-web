197f0a5de931ebd43fb09776dac38638a8a42d41
Akshat Sharma <akshatsharma46@gmail.com>
2019-04-25 18:48:33 -0700

    testing curl

--- curl.php
+++ curl.php
@@ -4,15 +4,16 @@
 
     include 'allUsers.php';
 
-    $ch = curl_init("http://brewgy.com/myUsers.php");
-    echo "testing 52";
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    echo "testing 54";
-    curl_setopt($ch, CURLOPT_BINARYTRANSFER, true);
-    echo "testing 56";
-    $content = curl_exec($ch);
-    echo "curl contnet:".$content;
-    curl_close($ch);
+    echo 'Hello World';
+    // $ch = curl_init("http://brewgy.com/myUsers.php");
+    // echo "testing 52";
+    // curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    // echo "testing 54";
+    // curl_setopt($ch, CURLOPT_BINARYTRANSFER, true);
+    // echo "testing 56";
+    // $content = curl_exec($ch);
+    // echo "curl contnet:".$content;
+    // curl_close($ch);
 
     ?>
 
